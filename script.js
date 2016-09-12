6273bf01c6a238229660fb8d11a1593b2b2fef34
chrismo1975 <christian.zender.1975@gmail.com>
2016-09-12 17:04:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,134 @@
+window.onload = function() {
+
+	// Video
+	var video = document.getElementById("video");
+
+	// Buttons
+	var playButton = document.getElementById("play-pause");
+	var muteButton = document.getElementById("mute");
+	var fullScreenButton = document.getElementById("full-screen");
+
+	// Sliders
+	var seekBar = document.getElementById("seek-bar");
+	var volumeBar = document.getElementById("volume-bar");
+
+
+	// Event listener for the play/pause button
+	playButton.addEventListener("click", function() {
+		if (video.paused == true) {
+			// Play the video
+			video.play();
+
+			// Update the button text to 'Pause'
+			playButton.innerHTML = "Pause";
+		} else {
+			// Pause the video
+			video.pause();
+
+			// Update the button text to 'Play'
+			playButton.innerHTML = "Play";
+		}
+	});
+
+	// Event listener for the play/pause button
+	video.addEventListener("click", function() {
+		if (video.paused == true) {
+			// Play the video
+			video.play();
+
+			// Update the button text to 'Pause'
+			playButton.innerHTML = "Pause";
+		} else {
+			// Pause the video
+			video.pause();
+
+			// Update the button text to 'Play'
+			playButton.innerHTML = "Play";
+		}
+
+		progress.setAttribute('max', video.duration);
+	});
+
+	// React to the user clicking within the progress bar
+	progress.addEventListener('click', function(e) {
+		//var pos = (e.pageX  - this.offsetLeft) / this.offsetWidth; // Also need to take the parent into account here as .controls now has position:relative
+		var pos = (e.pageX  - (this.offsetLeft + this.offsetParent.offsetLeft)) / this.offsetWidth;
+		video.currentTime = pos * video.duration;
+	});
+
+	// As the video is playing, update the progress bar
+	video.addEventListener('timeupdate', function() {
+		// For mobile browsers, ensure that the progress element's max attribute is set
+		if (!progress.getAttribute('max')) progress.setAttribute('max', video.duration);
+		progress.value = video.currentTime;
+		progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+	});
+	/* Event listener for the mute button
+	muteButton.addEventListener("click", function() {
+		if (video.muted == false) {
+			// Mute the video
+			video.muted = true;
+
+			// Update the button text
+			muteButton.innerHTML = "Unmute";
+		} else {
+			// Unmute the video
+			video.muted = false;
+
+			// Update the button text
+			muteButton.innerHTML = "Mute";
+		}
+	});
+	*/
+
+	/*
+	// Event listener for the full-screen button
+	fullScreenButton.addEventListener("click", function() {
+		if (video.requestFullscreen) {
+			video.requestFullscreen();
+		} else if (video.mozRequestFullScreen) {
+			video.mozRequestFullScreen(); // Firefox
+		} else if (video.webkitRequestFullscreen) {
+			video.webkitRequestFullscreen(); // Chrome and Safari
+		}
+	});
+	*/
+
+	/*
+	// Event listener for the seek bar
+	seekBar.addEventListener("change", function() {
+		// Calculate the new time
+		var time = video.duration * (seekBar.value / 100);
+
+		// Update the video time
+		video.currentTime = time;
+	});
+	*/
+	
+	// Update the seek bar as the video plays
+	video.addEventListener("timeupdate", function() {
+		// Calculate the slider value
+		var value = (100 / video.duration) * video.currentTime;
+
+		// Update the slider value
+		seekBar.value = value;
+	});
+
+	// Pause the video when the seek handle is being dragged
+	seekBar.addEventListener("mousedown", function() {
+		video.pause();
+	});
+
+	// Play the video when the seek handle is dropped
+	seekBar.addEventListener("mouseup", function() {
+		video.play();
+	});
+
+	/*
+	// Event listener for the volume bar
+	volumeBar.addEventListener("change", function() {
+		// Update the video volume
+		video.volume = volumeBar.value;
+	}); 
+	*/
+}
